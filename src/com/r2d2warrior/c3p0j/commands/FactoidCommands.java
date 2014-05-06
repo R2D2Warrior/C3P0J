e3c4af9287b47bedcf33b5b3aec2fd672b908d58
R2D2Warrior <r2d2warrior@hotmail.com>
2014-05-06 16:45:41 -0400

    Added factoid listing command + a TODO note

--- src/com/r2d2warrior/c3p0j/commands/FactoidCommands.java
+++ src/com/r2d2warrior/c3p0j/commands/FactoidCommands.java
@@ -1,6 +1,7 @@
 package com.r2d2warrior.c3p0j.commands;
 
 import java.sql.SQLException;
+import java.util.Set;
 
 import org.apache.commons.lang3.StringUtils;
 import org.pircbotx.PircBotX;
@@ -8,8 +9,10 @@ import org.pircbotx.PircBotX;
 import com.r2d2warrior.c3p0j.handling.CommandEvent;
 import com.r2d2warrior.c3p0j.handling.FactoidManager;
 
+//TODO sub-commands for this? .factoids add, .factoid del, .factoids list, etc
 @Command(name="remember", alt="r", desc="Saves, or \"remembers\", a factoid. Call with ?<factoidName>", syntax="remember <name> <data>", method="remember", requiresArgs=true, adminOnly=true)
 @Command(name="forget", alt="f", desc="Removes, or \"forgets\", a factoid", syntax="forget <factoidName>", method="forget", requiresArgs=true, adminOnly=true)
+@Command(name="factoids", desc="Lists all factoids.", method="list")
 public class FactoidCommands extends GenericCommand
 {
 	public FactoidCommands(CommandEvent<PircBotX> event)
@@ -64,5 +67,14 @@ public class FactoidCommands extends GenericCommand
 			e.printStackTrace();
 			event.respondToUser("An SQL error occurred while adding factoid.");
 		}
+	}
+	
+	public void list()
+	{
+		FactoidManager factoidManager = bot.getFactoidManager();
+
+		Set<String> factoidNames = factoidManager.getAllFactoids().keySet();
+		String factoidList = StringUtils.join(factoidNames, ", ");
+		event.respond("Available factoids: " + factoidList);
 	}
 }
