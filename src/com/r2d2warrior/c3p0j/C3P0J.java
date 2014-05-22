952d82a06701fc8b6d1439626d8d10ead30bb078
r2d2warrior <r2d2warrior@hotmail.com>
2014-05-21 22:20:20 -0400

    Updated example-config.json

--- src/com/r2d2warrior/c3p0j/C3P0J.java
+++ src/com/r2d2warrior/c3p0j/C3P0J.java
@@ -3,12 +3,7 @@ package com.r2d2warrior.c3p0j;
 import org.pircbotx.PircBotX;
 import org.pircbotx.Configuration;
 import org.pircbotx.cap.EnableCapHandler;
-import org.pircbotx.hooks.Listener;
-import org.reflections.Reflections;
-
-import com.r2d2warrior.c3p0j.listeners.AddListener;
 import com.r2d2warrior.c3p0j.utils.Config;
-import com.r2d2warrior.c3p0j.utils.Utils;
 
 public class C3P0J
 {
